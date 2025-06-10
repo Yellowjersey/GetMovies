0230cc6396be78a3f4982b0f3d52756d150efa45
Yellowjersey <bradfordgavin@gmail.com>
2025-06-10 15:57:15 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Search from './components/search';
+import Search from './components/Search';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
 import { getTrendingMovies, updateSearchCount } from './appwrite';
